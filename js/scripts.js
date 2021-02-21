eb0199cca69363cf2871b60e5f5d6c990aa99d7a
kelsey-n <kan317@nyu.edu>
2021-02-20 21:01:05 -0500

    added reset button and logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia25hbmFuIiwiYSI6ImNrbDlsMXNmNjI3MnEyb25yYjNre
 // 1. Initialize mapboxgl map and insert into mapcontainer div:
 var map = new mapboxgl.Map({
   container: 'mapcontainer', // container ID
-  style: 'mapbox://styles/mapbox/light-v10', // style URL
+  style: 'mapbox://styles/knanan/ckle1rswq26lu17paw71lych7', // style URL
   center: [-74.0060, 40.7128], // starting position [lng, lat]
   zoom: 12 // starting zoom
 });
@@ -18,7 +18,7 @@ var colordict = {
   "gray" : ['#A7A9AC', 'L'],
   "green" : ['#00933C', '4,5,6'],
   "lightgreen" : ['#6CBE45', 'G'],
-  "multiple" : ['black', 'multiple'],
+  "multiple" : ['black', 'multiple'], // 00FFFF- neon blueish
   "orange" : ['#FF6319', 'B,D,F,M'],
   "purple" : ['#B933AD', '7'],
   "red" : ['#EE352E', '1,2,3'],
@@ -27,6 +27,13 @@ var colordict = {
 }
 
 // 3. Create a legend that will also act as a filter - create buttons for each subway line, colored according to colordict
+
+//TESTING CODE for hover/focus/active logic to highlight button selection to user:
+// document.addEventListener('click', event => {
+//   if (event.target.matches('button')) {
+//     event.target.focus()
+//   }
+// })
 
 // first iterate over each key:value pair in the colordict object:
 $.each(colordict, function(colorkey) {
@@ -40,8 +47,10 @@ $.each(colordict, function(colorkey) {
       $('.first-station-buttons').append(`
         <button type="button" style="background-color:${colordict[colorkey][0]}" class = 'first-button' id='${subwayline}'>${subwayline}</button>`)
       $('.second-station-buttons').append(`
-        <button type="button" style="background-color:${colordict[colorkey][0]}" class = 'second-button' id='${subwayline}'>${subwayline}</button>`)
+        <button type="button" style="background-color:${colordict[colorkey][0]}" class = 'second-button' id='${subwayline}' disabled>${subwayline}</button>`)
     })
+    $('.first-station-buttons').append('<br/>')
+    $('.second-station-buttons').append('<br/>')
   }
 
 })
@@ -80,26 +89,43 @@ $.getJSON('./data/subwaystations.json', function(SubwayStations) {
   })
 
   // function to add/remove markers based on which button is clicked:
+
   $('.first-button').click(function() {
     var button_id = $(this).attr('id') // button id = subway line to filter on
     // search for the marker variable names that contain the button id subway line
-    marker_varnames.forEach(function(variable) {
-    if(variable.split('_')[1].includes(button_id)){window[variable].addTo(map)} // add markers that contain the button id's line to the map
-    else{window[variable].remove()} // remove markers that do not contain the button id's line
-    })
+    console.log(button_id)
+    if(button_id !=='reset-button'){
+      $('.second-button').prop("disabled", false);
+      $('.second-button').css("opacity", 1);
+      $('.second-button:hover').css("cursor", "pointer"); //NOT WORKING
+      marker_varnames.forEach(function(variable) {
+      if(variable.split('_')[1].includes(button_id)) {
+        window[variable].addTo(map)} // add markers that contain the button id's line to the map
+        else{window[variable].remove()} // remove markers that do not contain the button id's line
+      })
+    }
+
+    if(button_id==='reset-button'){
+      marker_varnames.forEach(function(variable) {window[variable].addTo(map)});
+      $('.second-button').prop("disabled", true);
+      $('.second-button').css("opacity", 0.6)
+    }
 
     $('.second-button').click(function() {
+      console.log(button_id)
+
       var button_id_2 = $(this).attr('id')
+      console.log(button_id_2)
       marker_varnames.forEach(function(variable) {
-      if(variable.split('_')[1].includes(button_id) && variable.split('_')[1].includes(button_id_2)){window[variable].addTo(map)}
-      else{window[variable].remove()}
-    })
+      if(variable.split('_')[1].includes(button_id) && variable.split('_')[1].includes(button_id_2)) {
+        window[variable].addTo(map)}
+        else{window[variable].remove()}
+      })
+  })
   })
 
-})
+  })
 
-})
-
-// may need to hard code in logic for stations like times sq, union sq, grand central, columbus circle, check for others
-// EXTRA TO DO'S: making second-stations unclickable until first-station is selected; only making one of each clickable;
-// highlighting buttons upon hover and selection
+// May need to hard code in logic for stations like times sq, union sq, grand central, columbus circle, atl ave, 149 st grand concourse, court sq, check for others
+/////// TO DO: to hardcode logic: change the line_varname; this way the color will still be based on the color column in the data.or make line_varname_2 with ALL lines at that station and use as butt id 2
+// EXTRA TO DO'S: keeping highlight on button that was selected
